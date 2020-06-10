2181eb8a08549a9b831546b194bf4bfcf44fcc8b
Amalgam0616 <56029851+Amalgam0616@users.noreply.github.com>
2020-06-10 11:45:45 +0900

    ぷるででたやつ

---
+++ Assets/Script/Sound/Fadein_Sound.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class Fadein_Sound : MonoBehaviour
+{
+    protected AudioSource Source;
+
+    private FadeManager FadeObj;
+
+    public bool IsFade;
+
+    public double FadeInSeconds = 1.0f;
+
+    double FadeDeltaTime = 0;
+
+    double BaseVolume;
+
+    void Start()
+    {
+        Source = this.GetComponent<AudioSource>();
+        BaseVolume = (float)PlayerPrefs.GetInt("VOLUME_BGM", 5);
+        //フェードパネルとUIの親取得
+        FadeObj = GameObject.Find("FadePanel").GetComponent<FadeManager>();
+    }
+
+    void Update()
+    {
+        Debug.Log("フェード");
+
+        if (IsFade)//&& !FadeObj.GetFadeOutFlg()
+        {
+            Debug.Log(BaseVolume);
+            Debug.Log(FadeInSeconds);
+            //Debug.Log(FadeDeltaTime);
+            Debug.Log(Source.volume);
+            FadeDeltaTime += Time.deltaTime;
+            if (FadeDeltaTime >= (FadeInSeconds * BaseVolume * 0.1))
+            {
+                FadeDeltaTime = FadeInSeconds * BaseVolume * 0.1;
+                IsFade = false;
+            }
+            Source.volume = (float)(FadeDeltaTime / FadeInSeconds) ;
+        }
+    }
+}
---
+++ Assets/Script/Sound/Fadeout_Sound.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class Fadeout_Sound : MonoBehaviour
+{
+    protected AudioSource Source;
+
+    public bool IsFade;
+
+    private FadeManager FadeObj;
+
+    public double FadeOutSeconds = 1.0;
+
+    bool IsFadeOut = true;
+
+    double BaseVolume_S;
+
+    double FadeDeltaTime = 0;
+
+    void Start()
+    {
+        Source = this.GetComponent<AudioSource>();
+
+        //フェードパネルとUIの親取得
+        FadeObj = GameObject.Find("FadePanel").GetComponent<FadeManager>();
+    }
+
+    void Update()
+    {
+        if (IsFadeOut && FadeObj.GetFadeOutFlg())
+        {
+            BaseVolume_S = (float)PlayerPrefs.GetInt("VOLUME_BGM", 5);
+
+            Debug.Log("フェードアウト開始");
+            Debug.Log(BaseVolume_S);
+            FadeDeltaTime += Time.deltaTime;
+            if (FadeDeltaTime >= FadeOutSeconds * BaseVolume_S * 0.1)
+            {
+                Debug.Log("フェードアウト終了");
+                FadeDeltaTime = FadeOutSeconds * BaseVolume_S * 0.1;
+                IsFadeOut = false;
+            }
+            Source.volume = (float)(1.0 - FadeDeltaTime / FadeOutSeconds);
+        }
+    }
+}
